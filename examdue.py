af39e30a4ff573b3dabed5bbf1a26ce15b8962c4
Nicholas Matteo <kundor@kundor.org>
2020-02-05 11:40:47 -0700

    Try extend_from_end_at instead of separate override

--- examdue.py
+++ examdue.py
@@ -2,9 +2,9 @@
 
 from canvas import *
 
-assid = 589988
-quizid = 85661
-date = '2020-02-05'
+assid = 589944
+quizid = 85617
+date = '2020-02-17'
 unlock = 'T19:00:00Z'
 due = 'T20:00:00Z'
 extradue = 'T20:30:00Z'
@@ -20,17 +20,12 @@ with canvas_session() as s:
         'assignment_override[unlock_at]': date + unlock})
     print(r)
     print(r.json())
-    r = s.post(curl, data={
-        'assignment_override[student_ids][]': extra_time_IDs,
-        'assignment_override[title]': '012 Extra time',
-        'assignment_override[due_at]': date + extradue,
-        'assignment_override[lock_at]': date + extradue,
-        'assignment_override[unlock_at]': date + unlock})
-    print(r)
-    print(r.json())
     for sid in extra_time_IDs:
+        # Is extend_from_end_at enough to add time to lock_at/due_at without creating a separate override?
+        # Needs to be tested
         curl = canvasbase + f'courses/{courseid}/quizzes/{quizid}/extensions'
         r = s.post(curl, data={
             'quiz_extensions[][user_id]': sid,
-            'quiz_extensions[][extra_time]': 25})
+            'quiz_extensions[][extra_time]': 25,
+            'quiz_extensions[][extend_from_end_at]': 30})
 
