b4630f91d8f3a4edcff180e14573ca8c6fc02505
Nicholas Matteo <kundor@kundor.org>
2020-02-20 11:15:30 -0700

    Get quiz ID arguments

--- check.py
+++ check.py
@@ -12,14 +12,26 @@ from traitlets.config import get_config
 
 sys.excepthook = IPython.core.ultratb.FormattedTB(mode='Verbose', color_scheme='Linux', call_pdb=1)
 
-if len(sys.argv <= 1):
-    sys.exit('Arguments: <submission zip file(s)> <original Module file>')
-if len(sys.argv) > 2:
-    subfiles = sys.argv[1:-1]
+if len(sys.argv <= 2):
+    sys.exit('Arguments: <quizid(s)> <submission zip file(s)> <original Module file>')
+arg = 1
+
+quizids = []
+while arg < len(sys.argv) and sys.argv[arg].isnumeric():
+    quizids.append(int(sys.argv[arg]))
+    arg += 1
+if not quizids:
+    sys.exit(f'Could not find any quiz IDs. Usage: {sys.argv[0]} <quizid(s)> <submission zip file(s)> <original Module file>')a
+
+subfiles = []
+if arg < len(sys.argv) - 1:
+    subfiles = sys.argv[arg:-1]
 else:
     subfiles = [os.path.expanduser('~/Downloads/submissions.zip')]
-    print(f'Using file {subfiles[0]}', file=sys.stderr)
+
 origfile = sys.argv[-1]
+
+print(f'Using quiz IDs {quizids}, submission zips {subfiles}, original file {origfile}', file=sys.stderr)
 
 Info = namedtuple('Info', ('filename', 'creation', 'creator', 'modified', 'modder')
 origcells = thecells(origfile)
