89bc956403823cfbba18e2a05f6b6af71ad793c7
Nicholas Matteo <kundor@kundor.org>
2020-02-25 14:05:04 -0700

    Autoselect exam in allgrades

--- allgrades.py
+++ allgrades.py
@@ -56,7 +56,14 @@ if __name__ == '__main__':
     except ValueError:
         sys.exit('Arguments must be quiz IDs (integers)')
     if not quizids:
-        sys.exit('Must specify at least one quiz ID')
+        exams = load_file('exams.json', json.load)
+        today = date.today()
+        theexams = [e for e in exams if e['date'] and isoparse(e['date']).date() == today]
+        if theexams:
+           print('Using assignments ' + ', '.join(e['name'] for e in theexams))
+           quizids = [e['quiz_id'] for e in theexams]
+        else:
+           sys.exit('Must specify at least one quiz ID')
 
     with canvas_session() as s:
         for quizid in quizids:
