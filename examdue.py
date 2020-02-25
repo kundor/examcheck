131b1ad0b8f47d522fc6cc036632dc08381f84f5
Nicholas Matteo <kundor@kundor.org>
2020-02-25 10:18:34 -0700

    Move exam due date setter into function

--- examdue.py
+++ examdue.py
@@ -2,35 +2,46 @@
 
 from canvas import *
 
-assid = 589988
-quizid = 85661
-date = '2020-02-05'
 unlock = 'T19:00:00Z'
 due = 'T20:00:00Z'
 extradue = 'T20:30:00Z'
 
-extra_time_IDs = [ 308978 ]
+# extra_time_IDs = [ 308978 ]
 
-with canvas_session() as s:
+def set_exam_due(session, assid, quizid, date, extra_time_IDs):
     curl = canvasbase + f'courses/{courseid}/assignments/{assid}/overrides'
-    r = s.post(curl, data={
+    response = s.post(curl, data={
         'assignment_override[course_section_id]': secid,
         'assignment_override[due_at]': date + due,
         'assignment_override[lock_at]': date + due,
         'assignment_override[unlock_at]': date + unlock})
-    print(r)
-    print(r.json())
-    r = s.post(curl, data={
+    print(response)
+    print(response.json())
+    if not extra_time_IDs:
+        return
+    response = s.post(curl, data={
         'assignment_override[student_ids][]': extra_time_IDs,
         'assignment_override[title]': '012 Extra time',
         'assignment_override[due_at]': date + extradue,
         'assignment_override[lock_at]': date + extradue,
         'assignment_override[unlock_at]': date + unlock})
-    print(r)
-    print(r.json())
+    print(response)
+    print(response.json())
     for sid in extra_time_IDs:
         curl = canvasbase + f'courses/{courseid}/quizzes/{quizid}/extensions'
         r = s.post(curl, data={
             'quiz_extensions[][user_id]': sid,
             'quiz_extensions[][extra_time]': 25})
 
+if __name__ == '__main__':
+    try:
+        assid = int(sys.argv[1])
+        quizid = int(sys.argv[2])
+        date = sys.argv[3]
+        if len(sys.argv) > 4:
+            extra_time_IDs = [int(arg) for arg in sys.argv[4:]]
+    except (ValueError, IndexError):
+        sys.exit(f'Usage: {sys.argv[0]} <assid> <quizid> <date> [extra-time IDs...]')
+
+    with canvas_session() as session:
+        set_exam_due(session, assid, quizid, date, extra_time_IDs)
