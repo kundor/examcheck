c0533d1200e16de8867e4b43a4b005a28d0d4d0b
Nicholas Matteo <kundor@kundor.org>
2020-04-07 13:57:38 -0600

    Follow next links

--- secdownload.py
+++ secdownload.py
@@ -53,40 +53,38 @@ with canvas_session() as s:
         signal.signal(signal.SIGINT, deferint)
 
         curi = (curi + 1) % len(curls)
-        with s.get(curls[curi]) as r:
-            rj = r.json()
-
-        if deferint.nomore:
-            break
-
-        for sub in rj:
-            sid = sub['user_id']
-            try:
-                stu = studict[sid]
-            except KeyError:
-                if sid not in weirdids:
-                    weirdids.add(sid)
-                    print('Wth?', sub)
-                continue
-            if 'attachments' not in sub:
-                continue
-            numatt = len(sub['attachments'])
-            if sid in dnlds:
-                if numatt != dnlds[sid]:
-                    print(f'{stu["name"]}: {numatt} attachments now, ?')
-                    dnlds[sid] = numatt
-                continue
-            if numatt != 1:
-                print(f'{stu["name"]}: {numatt} attachments, ?')
-            dst = f'{codename(stu)}_{sid}_' + sub['attachments'][-1]['filename']
-            url = sub['attachments'][-1]['url']
-            print('Fetching', dst, '... ', end='', flush=True)
-            with s.get(url, stream=True) as fileget, open(dst, 'wb') as out:
-                shutil.copyfileobj(fileget.raw, out)
-            dnlds[sid] = numatt
-            print(f'done {len(dnlds)}.')
+        for rj in follow_next(curls[curi]):
             if deferint.nomore:
                 break
+
+            for sub in rj:
+                sid = sub['user_id']
+                try:
+                    stu = studict[sid]
+                except KeyError:
+                    if sid not in weirdids:
+                        weirdids.add(sid)
+                        print('Wth?', sub)
+                    continue
+                if 'attachments' not in sub:
+                    continue
+                numatt = len(sub['attachments'])
+                if sid in dnlds:
+                    if numatt != dnlds[sid]:
+                        print(f'{stu["name"]}: {numatt} attachments now, ?')
+                        dnlds[sid] = numatt
+                    continue
+                if numatt != 1:
+                    print(f'{stu["name"]}: {numatt} attachments, ?')
+                dst = f'{codename(stu)}_{sid}_' + sub['attachments'][-1]['filename']
+                url = sub['attachments'][-1]['url']
+                print('Fetching', dst, '... ', end='', flush=True)
+                with s.get(url, stream=True) as fileget, open(dst, 'wb') as out:
+                    shutil.copyfileobj(fileget.raw, out)
+                dnlds[sid] = numatt
+                print(f'done {len(dnlds)}.')
+                if deferint.nomore:
+                    break
 
         elapsed = time.time() - start
         signal.signal(signal.SIGINT, signal.default_int_handler)
