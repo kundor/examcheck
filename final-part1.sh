470ca304e302c35cbccafaf90f01c707c702e01d
Nicholas Matteo <kundor@kundor.org>
2020-09-03 08:31:36 -0600

    Adjust final part 1 detection

--- final-part1.sh
+++ final-part1.sh
@@ -5,11 +5,11 @@ shopt -s extglob
 shopt -s nullglob
 shopt -s nocaseglob
 
-for student in $(ls *.xls* | sed 's/_.*//' | uniq -d)
+for student in $(ls *.xls* | sed -E 's/([0-9])_.*/\1/' | uniq -d)
 do
     didit=0
     fils=("$student"*.xls*)
-    twofils=("$student"_?(LATE_)+([0-9])_+([0-9])_*@(Part|P|pt|_|11)*(_|\ |-|.)2*.xls*)
+    twofils=("$student"_+([0-9])_*@(Part|P|pt|_|11)*(_|\ |-|.)2*.xls*)
     if [[ ${#twofils[@]} -gt 0 && ${#twofils[@]} -lt ${#fils[@]} ]]
     then
         echo "Deleting ${twofils[@]}"
