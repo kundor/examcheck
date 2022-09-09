037086ab1de65b8ff51f9bdeb49d52612eb366ff
Nicholas Matteo <kundor@kundor.org>
2022-09-09 13:57:01 -0600

    Script to see who hasn't started taking the exam yet

---
+++ secstat.py
@@ -0,0 +1,148 @@
+#!/home/kundor/.local/bin/ipython -i
+import os
+if 'DISPLAY' not in os.environ:
+    os.environ['DISPLAY'] = ':0'
+from canvas import *
+from collections import Counter
+
+quizid = 265038
+ignore_list = []
+secid = 83821
+
+#mysecs = [sections[si] for si in secids]
+mysec = sections[secid]
+secname = mysec['shortname']
+mystuds = mysec['students']
+studict = {stu["id"]: stu for stu in students}
+
+sesh = canvas_session()
+curl = canvasbase + f'courses/{courseid}/quizzes/{quizid}/submissions'
+
+
+def renew():
+    subs = []
+    for rj in follow_next(sesh, curl):
+        subs += rj['quiz_submissions']
+    return subs
+
+subs = renew()
+
+mysubs = [sub for sub in subs if sub['user_id'] in mystuds]
+
+statcounts = Counter(s['workflow_state'] for s in subs)
+
+print('Workflow stats (total):')
+for k in statcounts:
+    print(f'{k:>15}: {statcounts[k]}')
+
+mycounts = Counter(s['workflow_state'] for s in mysubs)
+
+print('In my sections:')
+for k in mycounts:
+    print(f'{k:>15}: {mycounts[k]}')
+
+alpha_list = sorted(mystuds, key=lambda s: studict[s]['sortable_name'])
+big_alpha = sorted((stu['id'] for stu in students), key=lambda s: studict[s]['sortable_name'])
+sub_dict = {sub['user_id'] : sub for sub in subs}
+
+def list_all():
+    n = 0
+    for sid in alpha_list:
+        if n % 30 == 1:
+            input('----More----')
+        n += 1
+        stu = studict[sid]
+        if 'email' in stu:
+            eml = stu['email']
+        else:
+            eml = stu['login_id'] + '@colorado.edu'
+        if sid not in sub_dict:
+            print(f'{stu["name"]:26} No submission' + ' '*23 + eml)
+        else:
+            sub = sub_dict[sid]
+            stat = sub['workflow_state']
+            if sub['started_at']:
+                _, start = sub['started_at'].split('T')
+            else:
+                start = '-'*9
+            if sub['end_at']:
+                _, end = sub['end_at'].split('T')
+            else:
+                end = '-'*9
+            print(f'{stu["name"]:26} {stat:14} {start} {end}  {eml}')
+
+def just_none():
+    n = 0
+    email_list = []
+    for sid in alpha_list:
+        if n and n % 35 == 0:
+            input('----More----')
+        stu = studict[sid]
+        if 'email' in stu:
+            eml = stu['email']
+        else:
+            eml = '(no email)' + stu['login_id'] + '@colorado.edu'
+        if sid not in sub_dict:
+            print(f'{stu["name"]:26} No submission' + ' '*23 + eml)
+            email_list.append(eml)
+            n += 1
+            continue
+        sub = sub_dict[sid]
+        stat = sub['workflow_state']
+        if stat in ('untaken', 'complete'):
+            continue
+        if sub['started_at']:
+            _, start = sub['started_at'].split('T')
+        else:
+            start = '-'*9
+        if sub['end_at']:
+            _, end = sub['end_at'].split('T')
+        else:
+            end = '-'*9
+        print(f'{stu["name"]:26} {stat:14} {start} {end}  {eml}')
+        email_list.append(eml)
+        n += 1
+    print(n, 'total')
+    return email_list
+
+elist = just_none()
+tl = [e for e in elist if not any(iname in e for iname in ignore_list)]
+print('; '.join(tl))
+
+
+def all_not():
+    email_list = []
+    for secid in sections:
+        if secid in secids:
+            continue
+        sec = sections[secid]
+        _, nam = sec['name'].split('-')
+        out = ''
+        for sid in sec['students']:
+            stu = studict[sid]
+            if 'email' in stu:
+                eml = stu['email']
+            else:
+                eml = stu['login_id'] + '@colorado.edu'
+            if sid not in sub_dict:
+                out += f'{stu["name"]:26}  {eml}\n'
+                email_list.append(eml)
+                continue
+            sub = sub_dict[sid]
+            stat = sub['workflow_state']
+            if stat in ('untaken', 'complete'):
+                continue
+            if sub['started_at']:
+                _, start = sub['started_at'].split('T')
+            else:
+                start = '-'*9
+            if sub['end_at']:
+                _, end = sub['end_at'].split('T')
+            else:
+                end = '-'*9
+            out += f'{stu["name"]:26}  {eml}\n'
+            email_list.append(eml)
+        if out:
+            print('*** Section', nam, '***')
+            print(out, end='')
+    return email_list
